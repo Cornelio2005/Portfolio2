e373393bd60027146846a3e154fd2d61ded4d980
Cornelio BENISSAN <corneliobenissan0@gmail.com>
2026-03-17 20:25:03 +0100

    Fin de la partie cv

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -86,10 +86,93 @@ function CV() {
                                 </div>
                              </div>
                              <h2 className="text-2xl font-bold text-white mb-4 mt-4">Liens utiles</h2>
-
+                                <div className="flex flex-wrap gap-2 mt-2">
+                                    <a href="https://github.com/Cornelio2005" target="_blank" className="btn  btn-outline rouded-full">
+                                        GitHub
+                                    </a>
+                                    <a href="https://linkedin.com/in/cornelio-benissan-847095388" target="_blank" className="btn  btn-outline rouded-full">
+                                        LinkedIn
+                                    </a>
+                                    <a href="/#projets"  className="btn  btn-outline rouded-full">
+                                        Mes Pojets 
+                                    </a>
+                                </div>
                         </div>
 
         </div>
+        {/* EXPÉRIENCE PROFESSIONNELLE */}
+        <div className="rounded-2xl p-8 mb-6 border border-white/10"
+         style={{ background: 'rgba(255,255,255,0.05)', backdropFilter: 'blur(10px)' }}
+            >
+                
+                <h2 className="text-2xl font-bold text-white mb-4">Expérience professionnelle</h2>
+                <div className="rounded-2xl p-6 border border-white/10 space-y-2">   
+                  <div className="flex justify-between items-start">
+                  <h3 className="text-lg font-bold text-white">Stagiaire en Développement Web · CEET</h3>
+                    <span className="badge badge-outline text-xs font-bold">STAGE</span>
+                  </div>
+
+                  <p className="text-sm text-base-content/50">
+    Lomé, Togo · Juin 2025 - Août 2025
+  </p>
+
+  {/* LIGNE 3 — Description */}
+  <p className="text-sm text-base-content/80">
+    Mise en place d'un Référentiel Centralistique Multi-SGBD. Analyse, conception UML, développement des modules et sécurisation des accès.
+  </p>
+
+  {/* LIGNE 4 — Compétences */}
+  <p className="text-sm text-base-content/50">
+    Compétences utilisées : PostgreSQL, PHP, Laravel 12
+  </p>
+            </div>
+                
+            </div>
+            {/* FORMATIONS */}
+                <div className="rounded-2xl p-8 mb-6 border border-white/10"
+                style={{ background: 'rgba(255,255,255,0.05)', backdropFilter: 'blur(10px)' }}>
+
+                <h2 className="text-2xl font-bold text-white mb-4">Formations</h2>
+
+                <div className="space-y-4">
+
+                    {/* ESGI */}
+                    <div className="rounded-2xl p-6 border border-white/10 space-y-1">
+                    <h3 className="text-lg font-bold text-white">ESGI Lyon</h3>
+                    <p className="text-secondary text-sm font-medium">
+                        Bachelor 2 en Informatique — Tronc Commun
+                    </p>
+                    <p className="text-sm text-base-content/50">
+                        Depuis septembre 2025
+                    </p>
+                    </div>
+
+                    {/* IAI TOGO */}
+                    <div className="rounded-2xl p-6 border border-white/10 space-y-1">
+                    <h3 className="text-lg font-bold text-white">IAI-TOGO — Lomé</h3>
+                    <p className="text-secondary text-sm font-medium">
+                        Licence 1 & Licence 2 en Informatique — Tronc Commun
+                    </p>
+                    <p className="text-sm text-base-content/50">
+                        Novembre 2023 - Août 2025
+                    </p>
+                    </div>
+
+                    {/* BAC */}
+                    <div className="rounded-2xl p-6 border border-white/10 space-y-1">
+                    <h3 className="text-lg font-bold text-white">
+                        Lycée Baguida-Plantation — Lomé, TOGO
+                    </h3>
+                    <p className="text-secondary text-sm font-medium">
+                        Baccalauréat Scientifique
+                    </p>
+                    <p className="text-sm text-base-content/50">
+                        Septembre 2022 - Juillet 2023
+                    </p>
+                    </div>
+
+                </div>
+                </div>
        
  
     </div>
